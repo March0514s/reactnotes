c7ba3037fca2d513f95f2e65f30d5ee58777b903
Marcus Davanço <md.daves@gmail.com>
2018-11-16 11:31:34 -0200

    Finished project

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyDGueLWVH44z6iiv91Ci2txvHqiVp4hIxY",
+    authDomain: "react-notes-94f12.firebaseapp.com",
+    databaseURL: "https://react-notes-94f12.firebaseio.com",
+    projectId: "react-notes-94f12",
+    storageBucket: "react-notes-94f12.appspot.com",
+    messagingSenderId: "892270234426"
+  };
